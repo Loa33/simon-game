6023b554c02eda4b38281e0921ff2beee696c14a
Loana <loana_valeyc@outloo.com>
2022-01-14 14:56:41 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+"use strict";
+let randomArray = [];
+let userArray = [];
+let colorsArray = ["green", "red", "yellow", "blue"];
+let level = 0;
+let flag = false;
+
+function generatePattern() {
+	userArray = [];
+	level++;
+	console.log(level);
+	$("h2").text("Level " + level);
+	let random = Math.floor(Math.random() * 3 + 1);
+	let randomColor = colorsArray[random];
+	randomArray.push(randomColor);
+	animateAndSound(randomColor);
+}
+
+$(".box").click(function () {
+	let userColor = $(this).attr("id");
+	userArray.push(userColor);
+	animateAndSound(userColor);
+	compare(userArray.length - 1);
+});
+
+function restart() {
+	level = 0;
+	randomArray = [];
+	flag = false;
+}
+
+$(document).keypress(function (e) {
+	if (!flag) {
+		generatePattern();
+		flag = true;
+	}
+});
+
+function compare(currentLevel) {
+	if (userArray[currentLevel] === randomArray[currentLevel]) {
+		if (userArray.length === randomArray.length) {
+			setTimeout(() => {
+				generatePattern();
+			}, 1000);
+		}
+	} else {
+		let audio;
+		audio = new Audio(`sounds/wrong.mp3`);
+		audio.muted = false;
+		audio.play();
+		$("body").addClass("game-over");
+		$("h2").text(`Game Over. Press any key to play again.`);
+		setTimeout(() => {
+			$("body").removeClass("game-over");
+		}, 200);
+		restart();
+	}
+}
+
+function animateAndSound(id) {
+	let audio;
+	audio = new Audio(`sounds/${id}.mp3`);
+	$(`#${id}`).animate({ opacity: 0.2 });
+	audio.muted = false;
+	audio.play();
+	setTimeout(function () {
+		$(`#${id}`).animate({ opacity: 1 });
+	}, 100);
+}
